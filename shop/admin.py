d1ba24dfe86a0029c6e51745142b3814d6ade4dc
KenajNazcok <86240942+KenajNazcok@users.noreply.github.com>
2024-11-10 00:30:18 +0100

    pep 8 - fix

--- shop/admin.py
+++ shop/admin.py
@@ -1,35 +1,45 @@
 from django.contrib import admin
-from .models import Product, Customer, Order, OrderItem, Payment, Cart, CartItem
+
+from .models import (Cart, CartItem, Customer, Order, OrderItem, Payment,
+                     Product)
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "description", "price", "stock")
     search_fields = ("name", "description")
 
+
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ("first_name", "email")
     search_fields = ("first_name", "email")
+
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ("customer", "created_at")
     list_filter = ("customer", "created_at")
     search_fields = ("customer__name",)
 
+
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ("order", "product", "quantity")
     list_filter = ("order",)
+
 
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ("order", "amount", "paid_at")
     list_filter = ("paid_at",)
     search_fields = ("paid_at__id",)
 
+
 class CartAdmin(admin.ModelAdmin):
     list_display = ("user", "created_at")
     search_fields = ("user__username",)
 
+
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ("cart", "product", "quantity")
     list_filter = ("cart", "product")
+
 
 # Rejestracja modeli
 admin.site.register(Product, ProductAdmin)
--- shop/views.py
+++ shop/views.py
@@ -1,42 +1,48 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.db import transaction
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from django.contrib.auth.decorators import user_passes_test, login_required
-from django.db import transaction
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
 from .forms import OrderForm, ProductForm
-from .models import Customer, Order, OrderItem, Payment, Product, Cart, CartItem
+from .models import (Cart, CartItem, Customer, Order, OrderItem, Payment,
+                     Product)
 
 
 def is_admin(user):
     return user.is_staff
 
+
 def user_register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, "Registration successful. You can now log in.")
-            return redirect('user_login')  # Po rejestracji przekierowanie do logowania
+            return redirect("user_login")  # Po rejestracji przekierowanie do logowania
         else:
             messages.error(request, "There was an error during registration.")
     else:
         form = UserCreationForm()
-    
-    return render(request, 'shop/register.html', {'form': form})
+
+    return render(request, "shop/register.html", {"form": form})
+
 
 def user_login(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
                 messages.success(request, "Logged in successfully!")
-                return redirect('product_list')  # Po udanym logowaniu przekierowujemy do listy produktów
+                return redirect(
+                    "product_list"
+                )  # Po udanym logowaniu przekierowujemy do listy produktów
             else:
                 messages.error(request, "Invalid username or password")
         else:
@@ -44,12 +50,14 @@ def user_login(request):
     else:
         form = AuthenticationForm()
 
-    return render(request, 'shop/login.html', {'form': form})
+    return render(request, "shop/login.html", {"form": form})
+
 
 def user_logout(request):
     logout(request)
     messages.success(request, "Logged out successfully!")
-    return redirect('product_list')
+    return redirect("product_list")
+
 
 @login_required
 def add_to_cart(request, product_id):
@@ -61,13 +69,13 @@ def add_to_cart(request, product_id):
         cart_item.quantity += 1
         cart_item.save()
 
-    return redirect('cart_detail')
+    return redirect("cart_detail")
 
 
 @login_required
 def cart_detail(request):
     cart, created = Cart.objects.get_or_create(user=request.user)
-    return render(request, 'shop/cart_detail.html', {'cart': cart})
+    return render(request, "shop/cart_detail.html", {"cart": cart})
 
 
 def product_list(request):
@@ -147,18 +155,20 @@ def create_order(request):
                     if isinstance(quantity, int):
                         product_quantities[product_id] = quantity
                     else:
-                        return HttpResponse("Invalid input for product quantity.", status=400)
+                        return HttpResponse(
+                            "Invalid input for product quantity.", status=400
+                        )
 
         except ValueError:
             return HttpResponse("Invalid input format.", status=400)
 
         try:
             customer = Customer.objects.get(id=customer_id)
-            
+
             # Tworzymy zamówienie w transakcji
             with transaction.atomic():
                 order = customer.place_order(product_quantities)
-            
+
             return redirect("order_detail", pk=order.id)
         except Customer.DoesNotExist:
             return HttpResponse("Customer does not exist.", status=400)
@@ -183,6 +193,7 @@ def process_payment(request, order_id):
             return HttpResponse("Insufficient amount to pay.", status=400)
     return render(request, "shop/process_payment.html", {"order": order})
 
+
 @login_required
 def checkout(request):
     cart, created = Cart.objects.get_or_create(user=request.user)
@@ -193,19 +204,17 @@ def checkout(request):
 
         # Tworzymy zamówienie
         order = Order.objects.create(customer=customer)
-        
+
         # Tworzymy przedmioty w zamówieniu
         for item in cart.items.all():
             OrderItem.objects.create(
-                order=order, 
-                product=item.product, 
-                quantity=item.quantity
+                order=order, product=item.product, quantity=item.quantity
             )
-        
+
         # Zwalniamy koszyk
         cart.items.all().delete()
-        
-        # Przekierowujemy do szczegółów zamówienia
-        return redirect('order_detail', pk=order.id)
 
-    return render(request, 'shop/checkout.html', {'cart': cart})
+        # Przekierowujemy do szczegółów zamówienia
+        return redirect("order_detail", pk=order.id)
+
+    return render(request, "shop/checkout.html", {"cart": cart})
